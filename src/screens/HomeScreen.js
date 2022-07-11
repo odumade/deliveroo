a418d50c101e13a5a3b96f58c02a12be1b1f31c9
odumade <104792577+odumade@users.noreply.github.com>
2022-07-11 10:42:54 +0100

    categories and category card set with screens and avatar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Platform, StyleSheet, Image, TextInput, ScrollView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+    UserIcon,
+    ChevronDownIcon,
+    AdjustmentsIcon,
+    SearchIcon
+} from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
+
     return (
-        <View>
-            <Text className="text-red-500">HomeScreen</Text>
-        </View>
+        <SafeAreaView style={styles.droidSafeArea}>
+            {/* Header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru',
+                    }}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+                    <Text className="font-bold text-sm">
+                        Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB" />
+                    </Text>
+                </View>
+                <UserIcon size={25} color="#00CCBB" />
+            </View>
+
+            {/* Search */}
+
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row flex-1 space-x-3  bg-gray-200 p-2">
+                    <SearchIcon color="gray" size={20} />
+                    <TextInput
+                        placeholder='Restaurants and Cuisine'
+                        keyboardType='default'
+                    />
+                </View>
+                <AdjustmentsIcon color="#00CCBB" />
+            </View>
+
+            {/* Body */}
+
+            <ScrollView>
+                {/* Categories */}
+
+                <Categories />
+
+
+                {/* Featured Rows */}
+            </ScrollView>
+
+        </SafeAreaView>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    droidSafeArea: {
+        paddingTop: Platform.OS === 'android' ? 30 : 0,
+        backgroundColor: "#ffffff",
+
+    }
+})
 
 export default HomeScreen
